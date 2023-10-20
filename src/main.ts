27fc8765994c637e143604e643305f37286502b7
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-10-20 15:56:33 +0100

    Initialization: Firebase and Firestore

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import * as serviceAccount from './config/serviceKey.json';
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
